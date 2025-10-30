46cc7bb13d7f83b1424e5b89e61f4d7a83228ad3
Zhafranabimata <adyaganteng981@gmail.com>
2025-10-30 12:06:10 +0700

    Barulagi

---
+++ JumlahKuadrat.java
@@ -0,0 +1,22 @@
+public class JumlahKuadrat {
+    public static void main(String[] args) {
+        
+        for (int n = 1; n <= 5; n++) {
+            int jumlah = 0;
+            String deret = "";
+
+            for (int i = 1; i <= n; i++) {
+                int kuadrat = i * i;
+                jumlah += kuadrat;
+
+                deret += kuadrat;
+                if (i < n) {
+                    deret += " + ";
+                }
+            }
+
+            System.out.println("n = " + n + " => jumlah kuadrat = " + deret + " = " + jumlah);
+        }
+    }
+}
+
--- NilaiKelompok29.java
+++ NilaiKelompok29.java
@@ -2,14 +2,21 @@ package dasprojobsheet7;
 import java.util.Scanner;
 public class NilaiKelompok29 {
     public static void main(String[] args) {
-        int i, j, nilai;
-        float totalNilai= 0, rataNilai= 0;
+        int i = 1, j, nilai;
+        float totalNilai, rataNilai= 0;
         Scanner sc = new Scanner(System.in);
 
-        for (j=1;j<=5;j++) { 
-            System.out.println("Nilai dari Kelompok Penilaian " +j+ ": ");
+        while (i<=6) {
+            System.out.println("Kelompok " +i);
+            totalNilai = 0;
+            for (j=1;j<=5;j++) { 
+                System.out.print("Nilai dari Kelompok Penilaian " +j+ ": ");
+                nilai = sc.nextInt();
+                totalNilai += nilai;
+            }
+            rataNilai = totalNilai/5;
+            System.out.println("Rata-rata dari Kelompok " +i+ ": " + rataNilai);
+            i++;  
         }
-        
-
     }
 }
